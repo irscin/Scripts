5c0d1d0c4f6687b443a5cb2f9270f2953ca686af
Italo Soares <irs@cin.ufpe.br>
2019-01-18 17:32:48 -0300

    Script for enabling GPIOs, enabling safe shutdown and updating

---
+++ install.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+
+#Step 1) Check if root--------------------------------------
+if [[ $EUID -ne 0 ]]; then
+   echo "Please execute script as root." 
+   exit 1
+fi
+#-----------------------------------------------------------
+
+#Step 2) enable UART----------------------------------------
+cd /boot/
+File=config.txt
+if grep -q "enable_uart=1" "$File";
+	then
+		echo "UART already enabled. Doing nothing."
+	else
+		echo "enable_uart=1" >> $File
+		echo "UART enabled."
+fi
+#-----------------------------------------------------------
+
+#Step 3) Update repository----------------------------------
+sudo apt-get update -y
+#-----------------------------------------------------------
+
+#Step 4) Install gpiozero module----------------------------
+sudo apt-get install -y python3-gpiozero
+#-----------------------------------------------------------
+
+#Step 5) Download Python script-----------------------------
+cd /opt/
+sudo mkdir RetroFlag
+cd /opt/RetroFlag
+script=SafeShutdown.py
+
+if [ -e $script ];
+	then
+		echo "Script SafeShutdown.py already exists. Doing nothing."
+	else
+		wget "https://raw.githubusercontent.com/irscin/Scripts/master/SafeShutdown.py"
+fi
+#-----------------------------------------------------------
+
+#Step 6) Enable Python script to run on start up------------
+cd /etc/
+RC=rc.local
+
+if grep -q "sudo python3 \/opt\/RetroFlag\/SafeShutdown.py \&" "$RC";
+	then
+		echo "File /etc/rc.local already configured. Doing nothing."
+	else
+		sed -i -e "s/^exit 0/sudo python3 \/opt\/RetroFlag\/SafeShutdown.py \&\n&/g" "$RC"
+		echo "File /etc/rc.local configured."
+fi
+#-----------------------------------------------------------
+
+#Step 7) Reboot to apply changes----------------------------
+echo "RetroFlag Pi Case installation done. Will now install updates"
+sleep 3
+#-----------------------------------------------------------
+#Step 8) Installing updates
+sudo apt update
+sudo apt upgrade
+#Step 9) Enabling SSH
+systemctl enable ssh.socket
+
+
